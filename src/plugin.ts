4aea26f3aadab60555e71db1dd01feef12739bd9
Heather <hkerri200@gmail.com>
2022-11-09 12:27:25 +0000

    add to export

--- src/plugin.ts
+++ src/plugin.ts
@@ -5,6 +5,7 @@ import {IDAManifestMetadata} from "./blocks/metadata/metadata";
 import {TopicHero} from "./blocks/custom-ida-hero/topic-hero";
 import {TopicAggHero} from "./blocks/custom-ida-hero/topic-agg-hero";
 import {FeedbackBtn} from "./blocks/feedback-btn/feedback-btn";
+import {idaTheme} from "./themes/ida-theme";
 
 export function hookRoutes() {
   return [
@@ -41,4 +42,5 @@ export const projectTemplates = [
 // All themes.
 export const themes = [
 //  exampleTheme
+    idaTheme
 ];
